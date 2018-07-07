46f23cefef13a3fb71f763196f6c2a8891a59fe2
TaiPtit <vuductaiptit@gmail.com>
2018-07-07 09:39:01 +0700

    "add heading text and css-Header page"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from "react"
+import logo from '../../static/img/logo-white.png'
 
 const Header = (props) => {
+    console.log(logo)
     return (
-        <React.Fragment>
-            somtext...
-        </React.Fragment>
+        <div className="header">
+            <div className="logo-box">
+                <img src={logo} alt="logo" className="logo" />
+            </div>
+            <div className="text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary-main">OUTDOORS</span>
+                    <span className="heading-primary-sub">IS WHERE LIFE HAPPENS</span>
+                </h1>
+            </div>
+        </div>
     );
 }
 
