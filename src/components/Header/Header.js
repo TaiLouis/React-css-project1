189efa6b6eddf847dd84b8117da2cf40a01c9e23
TaiPtit <vuductaiptit@gmail.com>
2018-07-07 13:35:57 +0700

    "convert pixel to rem and add header button "

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,6 +13,7 @@ const Header = (props) => {
                     <span className="heading-primary-main">OUTDOORS</span>
                     <span className="heading-primary-sub">IS WHERE LIFE HAPPENS</span>
                 </h1>
+                <a href="#" className="btn btn-white btn-animation">discover our tour</a>
             </div>
         </div>
     );
