f37b99763a8c067883a2c62cb106bb1ce70cc825
TaiPtit <vuductaiptit@gmail.com>
2018-07-08 09:08:05 +0700

    "set up using sass"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,15 +5,15 @@ const Header = (props) => {
     console.log(logo)
     return (
         <div className="header">
-            <div className="logo-box">
-                <img src={logo} alt="logo" className="logo" />
+            <div className="header__logo-box">
+                <img src={logo} alt="logo" className="header__logo" />
             </div>
-            <div className="text-box">
+            <div className="header__text-box">
                 <h1 className="heading-primary">
-                    <span className="heading-primary-main">OUTDOORS</span>
-                    <span className="heading-primary-sub">IS WHERE LIFE HAPPENS</span>
+                    <span className="heading-primary--main">OUTDOORS</span>
+                    <span className="heading-primary--sub">IS WHERE LIFE HAPPENS</span>
                 </h1>
-                <a href="#" className="btn btn-white btn-animation">discover our tour</a>
+                <a href="#" className="btn btn--white btn--animation">discover our tour</a>
             </div>
         </div>
     );
